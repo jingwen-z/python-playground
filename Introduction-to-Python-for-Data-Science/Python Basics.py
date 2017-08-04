27810edffa84e8212a54189fb1baf43b6772a4fd
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-08-04 23:35:32 +0200

    testing and datacamp course intro to python - Hello Python

---
+++ Introduction-to-Python-for-Data-Science/Python Basics.py
@@ -0,0 +1,33 @@
+### Hello Python
+
+## The Python Interface
+# Example, do not modify!
+print(5 / 8)
+
+# Put code below here
+print(7 + 10)
+
+## Any comments?
+# Just testing division
+print(5 / 8)
+
+# Addition works too
+print(7 + 10)
+
+## Python as a calculator
+# Addition and subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication and division
+print(3 * 5)
+print(10 / 2)
+
+# Exponentiation
+print(4 ** 2)
+
+# Modulo
+print(18 % 7)
+
+# How much is your $100 worth after 7 years?
+print(100 * 1.1 ** 7)
---
+++ test.py
@@ -0,0 +1,5 @@
+a = 1
+b = 2
+config = {'key1': 'val1', 'key2': 'val2'}
+print a
+print b
