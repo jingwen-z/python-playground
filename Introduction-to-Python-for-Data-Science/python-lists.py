2195e5d67382ff7a39506019c7022e2c9d1c8889
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-08-13 10:48:24 +0200

    List Manipulation

--- Introduction-to-Python-for-Data-Science/python-lists.py
+++ Introduction-to-Python-for-Data-Science/python-lists.py
@@ -51,3 +51,58 @@ print(areas[-1])
 
 # Print out the area of the living room
 print(areas[5])
+
+## Subset and calculate
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[-3]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+## Slicing and dicing
+# Use slicing to create downstairs
+downstairs = areas[0:6]
+
+# Use slicing to create upstairs
+upstairs = areas[6:10]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
+
+# Alternative slicing to create downstairs
+downstairs = areas[:6]
+
+# Alternative slicing to create upstairs
+upstairs = areas[6:]
+
+### List Manipulation
+
+## Replace list elements
+# Correct the bathroom area
+areas[-1] = 10.50
+
+# Change "living room" to "chill zone"
+areas[4] = "chill zone"
+
+## Extend a list
+# Add poolhouse data to areas, new list is areas_1
+areas_1 = areas + ["poolhouse", 24.5]
+
+# Add garage data to areas_1, new list is areas_2
+areas_2 = areas_1 + ["garage", 15.45]
+
+## Inner workings of lists
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Create areas_copy
+areas_copy = list(areas)
+# or
+# areas_copy = areas[:]
+
+# Change areas_copy
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
