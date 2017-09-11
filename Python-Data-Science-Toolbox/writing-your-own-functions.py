32eb8aa771b830cc1292e8410e278af2299f9305
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-09-11 22:50:34 +0200

    exercise of tuples

--- Python-Data-Science-Toolbox/writing-your-own-functions.py
+++ Python-Data-Science-Toolbox/writing-your-own-functions.py
@@ -77,3 +77,34 @@ yell = shout('congratulations', 'you')
 
 # Print yell
 print(yell)
+
+# A brief introduction to tuples
+# Unpack nums into num1, num2, and num3
+num1, num2, num3 = nums
+
+# Construct even_nums
+even_nums = 2, nums[1], nums[2]
+
+
+# Function that return multiple values
+# Define shout_all with parameters word1 and word2
+def shout_all(word1, word2):
+    # Concatenate word1 with '!!!': shout1
+    shout1 = word1 + '!!!'
+
+    # Concatenate word2 with '!!!': shout2
+    shout2 = word2 + '!!!'
+
+    # Construct a tuple with shout1 and shout2: shout_words
+    shout_words = shout1, shout2
+
+    # Return shout_words
+    return shout_words
+
+
+# Pass 'congratulations' and 'you' to shout_all(): yell1, yell2
+yell1, yell2 = shout_all('congratulations', 'you')
+
+# Print yell1 and yell2
+print(yell1)
+print(yell2)
