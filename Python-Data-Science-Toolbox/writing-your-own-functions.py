52b42ef75b39e791f9e25f3c0f4aff4250104e5f
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-09-18 12:12:12 +0200

    Bringing it all together - part 2

--- Python-Data-Science-Toolbox/writing-your-own-functions.py
+++ Python-Data-Science-Toolbox/writing-your-own-functions.py
@@ -132,3 +132,33 @@ for entry in col:
 
 # Print the populated dictionary
 print(langs_count)
+
+# Define count_entries()
+def count_entries(df, col_name) :
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Initialize an empty dictionary: langs_count
+    langs_count = {}
+
+    # Extract column from DataFrame: col
+    col = df[col_name]
+
+    # Iterate over lang column in DataFrame
+    for entry in col:
+
+        # If the language is in langs_count, add 1
+        if entry in langs_count.keys():
+            langs_count[entry] = langs_count[entry] + 1
+        # Else add the language to langs_count, set the value to 1
+        else:
+            langs_count[entry] = 1
+
+    # Return the langs_count dictionary
+    return(langs_count)
+
+# Call count_entries(): result
+result = count_entries(tweets_df, 'lang')
+
+# Print the result
+print(result)
