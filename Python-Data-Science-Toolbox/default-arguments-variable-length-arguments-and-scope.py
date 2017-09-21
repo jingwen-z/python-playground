fadfdb5e3ef0215d9c1bb8924fe79854ec0c135e
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-09-21 12:52:31 +0200

    Default and flexible arguments

--- Python-Data-Science-Toolbox/default-arguments-variable-length-arguments-and-scope.py
+++ Python-Data-Science-Toolbox/default-arguments-variable-length-arguments-and-scope.py
@@ -9,6 +9,7 @@ Scope and user-defined functions
 # Create a string: team
 team = "teen titans"
 
+
 # Define change_team()
 def change_team():
     # Use team in global scope
@@ -16,6 +17,7 @@ def change_team():
 
     # Change the value of team in global: team
     team = "justice league"
+
 
 # Print team
 print(team)
@@ -29,10 +31,11 @@ print(team)
 """
 Nested functions
 """
+
+
 # Nested Functions I
 # Define three_shouts
 def three_shouts(word1, word2, word3):
-
     # Define inner
     def inner(word):
         return word + '!!!'
@@ -40,8 +43,10 @@ def three_shouts(word1, word2, word3):
     # Return a tuple of strings
     return (inner(word1), inner(word2), inner(word3))
 
+
 # Call three_shouts() and print
 print(three_shouts('a', 'b', 'c'))
+
 
 # Nested Functions II
 # Define echo
@@ -67,6 +72,7 @@ thrice = echo(3)
 # Call twice() and thrice() then print
 print(twice('hello'), thrice('hello'))
 
+
 # The keyword nonlocal and nested functions
 # Define echo_shout()
 def echo_shout(word):
@@ -75,23 +81,137 @@ def echo_shout(word):
     # Concatenate word with itself: echo_word
     echo_word = word + word
 
-    #Print echo_word
+    # Print echo_word
     print(echo_word)
 
     # Define inner function shout()
     def shout():
         """Alter a variable in the enclosing scope"""
-        #Use echo_word in nonlocal scope
-        nonlocal echo_word
+        # Use echo_word in nonlocal scope
+        nonlocal
+        echo_word
 
-        #Change echo_word to echo_word concatenated with '!!!'
+        # Change echo_word to echo_word concatenated with '!!!'
         echo_word = echo_word + '!!!'
 
     # Call function shout()
     shout()
 
-    #Print echo_word
+    # Print echo_word
     print(echo_word)
 
-#Call function echo_shout() with argument 'hello'
+
+# Call function echo_shout() with argument 'hello'
 echo_shout('hello')
+
+"""
+Default and flexible arguments
+"""
+
+
+# Functions with one default argument
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+     exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+
+# Call shout_echo() with "Hey": no_echo
+no_echo = shout_echo('Hey')
+
+# Call shout_echo() with "Hey" and echo=5: with_echo
+with_echo = shout_echo('Hey', echo=5)
+
+# Print no_echo and with_echo
+print(no_echo)
+print(with_echo)
+
+
+# Functions with multiple default arguments
+# Define shout_echo
+def shout_echo(word1, echo=1, intense=False):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Capitalize echo_word if intense is True
+    if intense is True:
+        # Capitalize and concatenate '!!!': echo_word_new
+        echo_word_new = echo_word.upper() + '!!!'
+    else:
+        # Concatenate '!!!' to echo_word: echo_word_new
+        echo_word_new = echo_word + '!!!'
+
+    # Return echo_word_new
+    return echo_word_new
+
+
+# Call shout_echo() with "Hey", echo=5 and intense=True: with_big_echo
+with_big_echo = shout_echo('Hey', echo=5, intense=True)
+
+# Call shout_echo() with "Hey" and intense=True: big_no_echo
+big_no_echo = shout_echo('Hey', intense=True)
+
+# Print values
+print(with_big_echo)
+print(big_no_echo)
+
+
+# Function with variable-length arguments (*args)
+# Define gibberish
+def gibberish(*args):
+    """Concatenate strings in *args together."""
+
+    # Initialize an empty string: hodgepodge
+    hodgepodge = ''
+
+    # Concatenate the strings in args
+    for word in args:
+        hodgepodge += word
+
+    # Return hodgepodge
+    return hodgepodge
+
+
+# Call gibberish() with one string: one_word
+one_word = gibberish('luke')
+
+# Call gibberish() with five strings: many_words
+many_words = gibberish('luke', 'leia', 'han', 'obi', 'darth')
+
+# Print one_word and many_words
+print(one_word)
+print(many_words)
+
+
+# Function with variable-length keyword arguments (**kwargs)
+# Define report_status
+def report_status(**kwargs):
+    """Print out the status of a movie character."""
+
+    print("\nBEGIN: REPORT\n")
+
+    # Iterate over the key-value pairs of kwargs
+    for key, value in kwargs.items():
+        # Print out the keys and values, separated by a colon ':'
+        print(key + ": " + value)
+
+    print("\nEND REPORT")
+
+
+# First call to report_status()
+report_status(name='luke', affiliation='jedi', status='missing')
+
+# Second call to report_status()
+report_status(name='anakin', affiliation='sith lord', status='deceased')
