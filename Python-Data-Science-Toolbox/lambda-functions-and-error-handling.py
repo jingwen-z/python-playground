c9778d8efcdfd29032804fdc0b003d73600482f1
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-09-24 17:29:56 +0200

    Introduction to error handling

--- Python-Data-Science-Toolbox/lambda-functions-and-error-handling.py
+++ Python-Data-Science-Toolbox/lambda-functions-and-error-handling.py
@@ -48,3 +48,59 @@ result = reduce(lambda item1, item2: item1 + item2, stark)
 
 # Print the result
 print(result)
+
+"""
+Introduction to error handling
+"""
+
+
+# Error handling with try-except
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Initialize empty strings: echo_word, shout_words
+    echo_word = ''
+    shout_words = ''
+
+    # Add exception handling with try-except
+    try:
+        # Concatenate echo copies of word1 using *: echo_word
+        echo_word = word1 * echo
+
+        # Concatenate '!!!' to echo_word: shout_words
+        shout_words = echo_word + '!!!'
+    except:
+        # Print error message
+        print("word1 must be a string and echo must be an integer.")
+
+    # Return shout_words
+    return shout_words
+
+
+# Call shout_echo
+shout_echo("particle", echo="accelerator")
+
+
+# Error handling by raising an error
+# Define shout_echo
+def shout_echo(word1, echo=1):
+    """Concatenate echo copies of word1 and three
+    exclamation marks at the end of the string."""
+
+    # Raise an error with raise
+    if echo <= 0:
+        raise ValueError('echo must be greater than 0')
+
+    # Concatenate echo copies of word1 using *: echo_word
+    echo_word = word1 * echo
+
+    # Concatenate '!!!' to echo_word: shout_word
+    shout_word = echo_word + '!!!'
+
+    # Return shout_word
+    return shout_word
+
+
+# Call shout_echo
+shout_echo("particle", echo=5)
