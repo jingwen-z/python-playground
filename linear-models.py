a150a8dd438cbeeee8da4a22a05fbcb42ee79fe2
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2017-12-17 18:16:52 +0100

    Lasso regression

--- linear-models.py
+++ linear-models.py
@@ -7,7 +7,9 @@ Supervised Learning
 
 import matplotlib.pyplot as plt
 import mglearn.datasets
+import numpy as np
 from sklearn.datasets import load_boston
+from sklearn.linear_model import Lasso
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import train_test_split
@@ -86,4 +88,37 @@ plt.show()
 
 # learning curves for ridge regression and linear regression
 mglearn.plots.plot_ridge_n_samples()
+plt.show()
+
+"""
+Lasso
+"""
+# alpha = 1
+lasso = Lasso().fit(X_train, y_train)
+print('Training set score: {:.2f}'.format(lasso.score(X_train, y_train)))
+print('Test set score: {:.2f}'.format(lasso.score(X_test, y_test)))
+print('Number of features used: {}'.format(np.sum(lasso.coef_ != 0)))
+
+# alpha = 0.01
+lasso001 = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
+print('Training set score: {:.2f}'.format(lasso001.score(X_train, y_train)))
+print('Test set score: {:.2f}'.format(lasso001.score(X_test, y_test)))
+print('Number of features used: {}'.format(np.sum(lasso001.coef_ != 0)))
+
+# alpha = 0.0001
+lasso00001 = Lasso(alpha=0.0001, max_iter=100000).fit(X_train, y_train)
+print('Training set score: {:.2f}'.format(lasso00001.score(X_train, y_train)))
+print('Test set score: {:.2f}'.format(lasso00001.score(X_test, y_test)))
+print('Number of features used: {}'.format(np.sum(lasso00001.coef_ != 0)))
+
+# compare coefficient magnitudes
+plt.plot(lasso.coef_, 's', label='Lasso alpha=1')
+plt.plot(lasso001.coef_, '^', label='Lasso alpha=0.01')
+plt.plot(lasso00001.coef_, 'v', label='Lasso alpha=0.0001')
+plt.plot(ridge01.coef_, 'o', label='Lasso alpha=0.1')
+
+plt.xlabel('Coefficient index')
+plt.ylabel('Coefficient magnitude')
+plt.ylim(-25, 25)
+plt.legend(ncol=2, loc=(0, 1.05))
 plt.show()
