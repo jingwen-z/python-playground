46e0b920f6e6a8bb80a399e15412ecdf4ac36e97
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2018-01-01 19:48:51 +0100

    feature importances with breast cancer dataset

--- decision_trees.py
+++ decision_trees.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 """
 Introduction to Machine Learning with Python
 Chapter 2
@@ -7,13 +8,13 @@ Supervised Learning - Decision Trees
 """
 
 import graphviz
+import matplotlib.pyplot as plt
 import mglearn.datasets
+import numpy as np
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.tree import export_graphviz
-
-mglearn.plots.plot_animal_tree()
 
 """
 Controlling complexity of decision trees
@@ -21,10 +22,10 @@ Controlling complexity of decision trees
 cancer = load_breast_cancer()
 x_train, x_test, y_train, y_test = train_test_split(cancer.data, cancer.target, stratify=cancer.target, random_state=42)
 
-# tree = DecisionTreeClassifier(random_state=0)
-# tree.fit(x_train, y_train)
-# print('Accuracy on training set: {:.3f}'.format(tree.score(x_train, y_train)))
-# print('Accuracy on test set: {:.3f}'.format(tree.score(x_test, y_test)))
+tree = DecisionTreeClassifier(random_state=0)
+tree.fit(x_train, y_train)
+print('Accuracy on training set: {:.3f}'.format(tree.score(x_train, y_train)))
+print('Accuracy on test set: {:.3f}'.format(tree.score(x_test, y_test)))
 
 tree = DecisionTreeClassifier(max_depth=4, random_state=0)
 tree.fit(x_train, y_train)
@@ -41,3 +42,23 @@ with open('tree.dot') as f:
     dot_graph = f.read()
 
 graphviz.Source(dot_graph).view()
+
+"""
+Feature importance in trees
+"""
+
+
+def plot_feature_importances_cancer(model):
+    n_features = cancer.data.shape[1]
+    plt.barh(range(n_features), model.feature_importances_, align='center')
+    plt.yticks(np.arange(n_features), cancer.feature_names)
+    plt.xlabel('Feature importance')
+    plt.ylabel('Feature')
+    plt.show()
+
+
+plot_feature_importances_cancer(tree)
+print('Feature importances:\n{}'.format(tree.feature_importances_))
+
+tree = mglearn.plots.plot_tree_not_monotone()
+print(tree)
