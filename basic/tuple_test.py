10ae1b6808cbc0ddc5b215f468880d2e795fb159
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2018-03-10 18:17:13 +0100

    add init, update test_unpack_iter

--- basic/tuple_test.py
+++ basic/tuple_test.py
@@ -54,9 +54,10 @@ class TestUnpackingTuple(unittest.TestCase):
 
     def test_unpack_iter(self):
         seq = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+        result = ""
         for a, b, c in seq:
-            print('a = {0}, b = {1}, c = {2}'.format(a, b, c))
-        self.assertEqual(7, a)
+            result += 'a={0}, b={1}, c={2}; '.format(a, b, c)
+        self.assertEqual(result, 'a=1, b=2, c=3; a=4, b=5, c=6; a=7, b=8, c=9; ')
 
     def test_rest(self):
         values = 1, 2, 3, 4, 5
