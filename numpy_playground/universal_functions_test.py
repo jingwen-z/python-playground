ebc564da60f6159f9209b61abb0badd728b111a3
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2018-03-29 22:41:20 +0200

    unary ufuncs - abs, square, sign, ceil, floor

--- numpy_playground/universal_functions_test.py
+++ numpy_playground/universal_functions_test.py
@@ -26,6 +26,27 @@ class TestUniversalFunctions(unittest.TestCase):
         np.sqrt(arr, arr)
         self.assertTrue((np.array([0, 1, 0.2]) == arr).all())
 
+    def test_abs(self):
+        arr = np.array([2, 3.6, -5, -2.7])
+        self.assertTrue((np.array([2, 3.6, 5, 2.7]) == np.abs(arr)).all())
+        self.assertTrue((np.array([2, 3.6, 5, 2.7]) == np.fabs(arr)).all())
+
+    def test_square(self):
+        arr = np.array([0, 1, 2])
+        self.assertTrue((np.array([0, 1, 4]) == np.square(arr)).all())
+
+    def test_sign(self):
+        arr = np.array([-2, 0, 2])
+        self.assertTrue((np.array([-1, 0, 1]) == np.sign(arr)).all())
+
+    def test_ceil(self):
+        arr = np.array([-2.2, 0, 2.2])
+        self.assertTrue((np.array([-2, 0, 3]) == np.ceil(arr)).all())
+
+    def test_floor(self):
+        arr = np.array([-2.2, 0, 2.2])
+        self.assertTrue((np.array([-3, 0, 2]) == np.floor(arr)).all)
+
 
 if __name__ == '__main__':
     unittest.main()
