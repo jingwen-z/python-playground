f73a6d38a3ff7e3daa72fa82fee95f252a72d6cd
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2018-06-05 22:31:03 +0200

    learn series and df arithmetic

--- pandas_playground/arithmetic_test.py
+++ pandas_playground/arithmetic_test.py
@@ -26,6 +26,31 @@ class TestArithmetic(unittest.TestCase):
         self.assertFalse(math.isnan(dftotal.loc['b', 'b']))
         self.assertEqual(3, dftotal.loc['b', 'b'])
 
+    def test_fill_value(self):
+        df1 = pd.DataFrame(np.arange(4).reshape(2, 2),
+                           index=['a', 'b'], columns=list('bc'))
+        df2 = pd.DataFrame(np.arange(6).reshape(3, 2),
+                           index=['b', 'c', 'd'], columns=list('ab'))
+        df3 = df1.add(df2, fill_value=0)
+
+        self.assertTrue(math.isnan(df3.loc['a', 'a']))
+        self.assertEqual(2 + 1, df3.loc['b', 'b'])
+        self.assertEqual(0 + 0, df3.loc['a', 'b'])
+
+    def test_division_flipped(self):
+        df = pd.DataFrame(np.arange(4).reshape(2, 2),
+                          index=['a', 'b'], columns=list('bc'))
+        pd.testing.assert_frame_equal(1 / df, df.rdiv(1))
+
+    def test_ope_df_series(self):
+        df = pd.DataFrame(np.arange(6).reshape(3, 2),
+                          index=['a', 'b', 'c'], columns=list('bc'))
+        obj = df.iloc[0]
+        expected = pd.DataFrame({'b': [0, 2, 4], 'c': [0, 2, 4]},
+                                index=['a', 'b', 'c'])
+
+        pd.testing.assert_frame_equal(expected, df - obj)
+
 
 if __name__ == '__main__':
     unittest.main()
