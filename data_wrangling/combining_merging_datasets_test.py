0193827201905be41001a3faa49b328daa4a718e
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2018-08-19 22:08:12 +0200

    combining and merging datasets

--- data_wrangling/combining_merging_datasets_test.py
+++ data_wrangling/combining_merging_datasets_test.py
@@ -15,6 +15,12 @@ S1 = pd.Series([0, 1], index=['a', 'b'])
 S2 = pd.Series([2, 3, 4], index=['c', 'd', 'e'])
 S3 = pd.Series([5, 6], index=['f', 'g'])
 
+A = pd.Series([NA, 2.5, 0.0, 3.5, 4.5, NA],
+              index=['f', 'e', 'd', 'c', 'b', 'a'])
+
+B = pd.Series([0., NA, 2., NA, NA, 5.],
+              index=['a', 'b', 'c', 'd', 'e', 'f'])
+
 
 class TestMergingOnIndex(unittest.TestCase):
     def test_right_index(self):
@@ -68,8 +74,32 @@ class TestConcatenating(unittest.TestCase):
 
         self.assertTrue((concatenated == expected).all())
 
-    def test_pandas_concat(self):
+    def test_pandas_concat_rows(self):
+        concatenated = pd.concat([S1, S2, S3])
+        expected = pd.Series([0, 1, 2, 3, 4, 5, 6],
+                             index=['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+        pd.testing.assert_series_equal(concatenated, expected)
 
+    def test_pandas_concat_columns(self):
+        concatenated = pd.concat([S1, S2, S3], axis=1)
+        expected = pd.DataFrame({0: [0.0, 1.0, NA, NA, NA, NA, NA],
+                                 1: [NA, NA, 2.0, 3.0, 4.0, NA, NA],
+                                 2: [NA, NA, NA, NA, NA, 5.0, 6.0]},
+                                columns=range(0, 3),
+                                index=['a', 'b', 'c', 'd', 'e', 'f', 'g'])
+        pd.testing.assert_frame_equal(concatenated, expected)
+
+
+class TestCombiningwithOverlap(unittest.TestCase):
+    def test_np_where(self):
+        combined = np.where(pd.isnull(A), B, A)
+        self.assertTrue(
+            (combined == np.array([0., 2.5, 0., 3.5, 4.5, 5.])).all())
+
+    def test_combine_first(self):
+        expected = pd.Series([0.0, 4.5, 2.0, 0.0, 2.5, 5.0],
+                             index=['a', 'b', 'c', 'd', 'e', 'f'])
+        pd.testing.assert_series_equal(B.combine_first(A), expected)
 
 
 if __name__ == '__main__':
