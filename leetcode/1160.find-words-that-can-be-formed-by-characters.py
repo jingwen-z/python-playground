17d31e9e858af1210be73662f7b6d5b743267028
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2020-01-23 22:19:53 +0100

    1160

---
+++ leetcode/1160.find-words-that-can-be-formed-by-characters.py
@@ -0,0 +1,42 @@
+# Easy
+#
+# You are given an array of strings words and a string chars.
+#
+# A string is good if it can be formed by characters from chars (each character
+# can only be used once).
+#
+# Return the sum of lengths of all good strings in words.
+#
+# Example 1:
+#
+# Input: words = ["cat","bt","hat","tree"], chars = "atach"
+# Output: 6
+# Explanation:
+# The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+#
+# Example 2:
+#
+# Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+# Output: 10
+# Explanation:
+# The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+#
+# Note:
+#
+# 1 <= words.length <= 1000
+# 1 <= words[i].length, chars.length <= 100
+# All strings contain lowercase English letters only.
+
+class Solution:
+    def countCharacters(self, words: List[str], chars: str) -> int:
+        output = 0
+        for word in words:
+            chars_cp = chars
+            for char in word:
+                if char in chars_cp:
+                    chars_cp = chars_cp.replace(char, '', 1)
+                else:
+                    break
+                if len(chars)-len(chars_cp) == len(word):
+                    output += len(word)
+        return output
