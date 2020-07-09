3d9e9b969e8d202136ebd5154fbe8a6e410b8414
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2020-07-09 09:09:36 +0200

    add comments

--- learn_unittest_mock/test_assertions_and_inspection.py
+++ learn_unittest_mock/test_assertions_and_inspection.py
@@ -10,8 +10,11 @@ print(json)
 class AssertionsTests(unittest.TestCase):
     def test_assertions(self):
         self.assertEqual(1, json.loads.call_count)
+        # the last loads() call
         self.assertEqual(call({'key': 'value'}), json.loads.call_args)
+        # list of loads() calls
         self.assertEqual([call({'key': 'value'})], json.loads.call_args_list)
+        # list of calls to json's methods (recursively)
         self.assertEqual([call.loads({'key': 'value'})], json.method_calls)
 
         json.loads.assert_called()
